0b6b982f0a00f6b7ab7e69b146d64343248e2656
Olesya Kuznetsova <73638532+olemika@users.noreply.github.com>
2020-11-25 11:28:05 +0300

    3 commit
    - другой апи
    - подробности на сегодня
    - адаптив
    - стили

--- src/Today.js
+++ src/Today.js
@@ -8,32 +8,71 @@ const Today = () => {
     const key = '46ff5a3df8a8bb470a1eba7f5d0ceed7'
     const baseUrl = 'https://api.openweathermap.org/data/2.5'
 
-    
-    
+
     useEffect(() => {
 
-        fetch(`${baseUrl}/weather?id=524901&units=metric&appid=${key}`)
+        fetch(`${baseUrl}/onecall?lat=55.753960&lon=37.620393&exclude=minutely,hourly&units=metric&appid=46ff5a3df8a8bb470a1eba7f5d0ceed7`)
             .then(data => data.json())
-            .then(data =>    setWeather({
-                temp: Math.floor(data.main.temp),
-                feelslike: Math.floor(data.main.feels_like),
-                icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` ,
-                humidity: data.main.humidity,
-           }
-    ))
+            .then(data => setWeather({
+                currentTemp: Math.floor(data.current.temp),
+                currentFeelsLike: Math.floor(data.current.feels_like),
+                icon: `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`,
+                humidity: data.current.humidity,
 
-       
+                tempDay: Math.floor(data.daily[0].temp.day),
+                feelslikeDay: Math.floor(data.daily[0].feels_like.day),
+
+                tempNight: Math.floor(data.daily[0].temp.night),
+                feelslikeNight: Math.floor(data.daily[0].feels_like.night),
+
+                tempEve: Math.floor(data.daily[0].temp.eve),
+                feelslikeEve: Math.floor(data.daily[0].feels_like.eve),
+
+                tempMorn: Math.floor(data.daily[0].temp.morn),
+                feelslikeMorn: Math.floor(data.daily[0].feels_like.morn),
+
+
+            }
+            ))
+
+
     }, [])
 
     return (
         <div className="forecast">
-        <h2>Сегодняшняя погода</h2>
-            <div className="item">
-                <img src={weather.icon} />
-                <p>Сегодня</p>
-                <p>Температура: {weather.temp} ℃ </p>
-                <p>Ощущается как: {weather.feelslike} ℃ </p>
-                <p>Влажность: {weather.humidity} % </p>
+            <h2>Погода в Москве сегодня</h2>
+            <div className="todayList">
+                <div className="item todayMain">
+                    <img src={weather.icon} />
+                    <h3>Сейчас</h3>
+                    <p>Температура: {weather.tempMorn} ℃ </p>
+                    <p>Ощущается как: {weather.feelslikeMorn} ℃ </p>
+                    <p>Влажность: {weather.humidity} % </p>
+                </div>
+                <div className="item mini">
+                    <h3>Сегодня утром</h3>
+                    <p>Температура: {weather.tempMorn} ℃ </p>
+                    <p>Ощущается как: {weather.feelslikeMorn} ℃ </p>
+                    <p>Влажность: {weather.humidity} % </p>
+                </div>
+                <div className="item mini">
+                    <h3>Сегодня днем</h3>
+                    <p>Температура: {weather.tempDay} ℃ </p>
+                    <p>Ощущается как: {weather.feelslikeDay} ℃ </p>
+                    <p>Влажность: {weather.humidity} % </p>
+                </div>
+                <div className="item mini">
+                    <h3>Сегодня вечером</h3>
+                    <p>Температура: {weather.tempEve} ℃ </p>
+                    <p>Ощущается как: {weather.feelslikeEve} ℃ </p>
+                    <p>Влажность: {weather.humidity} % </p>
+                </div>
+                <div className="item mini">
+                    <h3>Сегодня ночью</h3>
+                    <p>Температура: {weather.tempNight} ℃ </p>
+                    <p>Ощущается как: {weather.feelslikeNight} ℃ </p>
+                    <p>Влажность: {weather.humidity} % </p>
+                </div>
             </div>
         </div>
     )
--- src/Week.js
+++ src/Week.js
@@ -6,8 +6,6 @@ const Week = () => {
     const [weather, setWeather] = useState('')
 
     
-    // const baseUrl = 'https://api.openweathermap.org/data/2.5'
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=55.753960&lon=37.620393&exclude=minutely,hourly&units=metric&appid=46ff5a3df8a8bb470a1eba7f5d0ceed7`)
             .then(data => data.json())
@@ -23,11 +21,11 @@ const Week = () => {
     
 
         <div className="forecast">
-            <h2>Погода на неделю</h2>
+            <h2>Погода в Москве на неделю</h2>
             <div className='week'>
                 {weather.length > 0 && weather.map(d => <div className='item'> 
                 <img src={`http://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`} />  
-                <p>Дата: {getTime(d.dt)}</p>
+                <h3>Дата: {getTime(d.dt)}</h3>
                 <p>Температура: {Math.floor(d.temp.day)} ℃ </p>
                 <p> Ощущается как: {Math.floor(d.feels_like.day)} ℃ </p>
                 <p> Влажность: {d.humidity} % </p>
